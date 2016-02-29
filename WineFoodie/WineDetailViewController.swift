7e05e10cfd99c1c858d4fa9df7aec12afbac9330
Maria Civilis <maria.civilis@gmail.com>
2016-02-29 17:24:34 -0500

    Wine detail view controller initial setup.

---
+++ WineFoodie/WineDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WineDetailViewController.swift
+//  WineFoodie
+//
+//  Created by Maria Civilis on 2016-02-29.
+//  Copyright © 2016 mcivilis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class WineDetailViewController : UIViewController {
+    
+}
