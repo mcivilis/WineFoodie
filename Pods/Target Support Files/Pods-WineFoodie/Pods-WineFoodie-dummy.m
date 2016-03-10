3e17ad0daff65b1c88b6248ebc7141f034f96496
Maria Civilis <maria.civilis@gmail.com>
2016-03-10 11:28:48 -0500

    Added a readme

---
+++ Pods/Target Support Files/Pods-WineFoodie/Pods-WineFoodie-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WineFoodie : NSObject
+@end
+@implementation PodsDummy_Pods_WineFoodie
+@end
---
+++ WineFoodie/FoodPair.swift
@@ -0,0 +1,12 @@
+//
+//  FoodPair.swift
+//  WineFoodie
+//
+//  Created by Maria Civilis on 2016-03-10.
+//  Copyright © 2016 mcivilis. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+
