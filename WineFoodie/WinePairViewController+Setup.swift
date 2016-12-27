cbdededf198fa18befc5fadf2114735a6d7e0bb2
Maria Civilis <maria.civilis@gmail.com>
2016-12-27 14:39:00 -0500

    add bw images and insure to start with non empty list

--- WineFoodie/WinePairViewController+Setup.swift
+++ WineFoodie/WinePairViewController+Setup.swift
@@ -13,10 +13,48 @@ extension WinePairViewController {
     
     func setup() {
         saveLabels()
-        selectCurrentLabel(redLabel)
+        
+        if (self.wineGroupHasMatch(.Red)) {
+            selectCurrentLabel(redLabel)
+            self.currentWineGroup = .Red
+        } else if (self.wineGroupHasMatch(.White)) {
+            selectCurrentLabel(whiteLabel)
+            self.currentWineGroup = .White
+        } else if (self.wineGroupHasMatch(.Rose)) {
+            selectCurrentLabel(roseLabel)
+            self.currentWineGroup = .Rose
+        } else if (self.wineGroupHasMatch(.Dessert)) {
+            selectCurrentLabel(dessertLabel)
+            self.currentWineGroup = .Dessert
+        } else if (self.wineGroupHasMatch(.Sparkling)) {
+            selectCurrentLabel(sparklingLabel)
+            self.currentWineGroup = .Sparkling
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Could not load wine list", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            alert.view.tintColor = kWineFoodie.colors.burgundy
+        }
+        
         dataManager.delegate = self
         tableView.estimatedRowHeight = 44
         tableView.rowHeight = UITableViewAutomaticDimension
+        
+        if (!self.wineGroupHasMatch(.Red)) {
+            self.redImage.image = UIImage.init(named: "red_bw")
+        }
+        if (!self.wineGroupHasMatch(.White)) {
+            self.redImage.image = UIImage.init(named: "white_bw.png")
+        }
+        if (!self.wineGroupHasMatch(.Rose)) {
+            self.redImage.image = UIImage.init(named: "rose_bw.png")
+        }
+        if (!self.wineGroupHasMatch(.Sparkling)) {
+            self.redImage.image = UIImage.init(named: "sparkling_bw.png")
+        }
+        if (!self.wineGroupHasMatch(.Dessert)) {
+            self.redImage.image = UIImage.init(named: "dessert_bw.png")
+        }
     }
 
 }
